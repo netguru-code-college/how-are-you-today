48775182165a42f8546a9aef3f1cfed14d3a1983
ritaly <malgorzata.lyczywek@gmail.com>
2018-05-26 14:32:43 +0200

    allow only for verified users

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,15 +4,17 @@ module ApplicationCable
 
     def connect
       self.current_user = find_verified_user
+      logger.add_tags 'ActionCable', current_user.email
     end
 
-    private
-      def find_verified_user
-        if verified_user = User.find_by(id: cookies.encrypted[:user_id])
-          verified_user
-        else
-          reject_unauthorized_connection
-        end
+    protected
+
+    def find_verified_user
+      if verified_user == env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
       end
+    end
   end
 end
