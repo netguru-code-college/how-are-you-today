b57d0308796c0ab404cbbec8c5d854c953838ebe
ritaly <malgorzata.lyczywek@gmail.com>
2018-05-26 11:52:33 +0200

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-
+  WebNotificationsChannel.broadcast_to(
+  current_user,
+  title: 'New things!',
+  body: 'All the news fit to print'
+	)
 end
