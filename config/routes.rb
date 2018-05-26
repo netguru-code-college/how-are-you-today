0d37972630152ea944b59df65539706f42bfef43
ritaly <malgorzata.lyczywek@gmail.com>
2018-05-26 14:06:48 +0200

    modify routes for cable

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  resources :chat_rooms, only: [:new, :create, :show, :index]
+  resources :chat_rooms, only: %i[new create show index]
+  mount ActionCable.server => '/cable'
 end
