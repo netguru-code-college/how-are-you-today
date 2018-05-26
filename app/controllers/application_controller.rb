ef2c7cf3c81f20df0ac1d4cfcff2313f8599eb6e
ritaly <malgorzata.lyczywek@gmail.com>
2018-05-26 12:36:35 +0200

    add model chatrooms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  WebNotificationsChannel.broadcast_to(
-  current_user,
-  title: 'New things!',
-  body: 'All the news fit to print'
-	)
+  before_action :authenticate_user!
 end
