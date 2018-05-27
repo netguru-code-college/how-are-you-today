a9b405efcf1b68c2f318e879197ce8a41a0d1574
Michał Łasecki <michal.lasecki@firma.interia.pl>
2018-05-27 13:43:11 +0200

    fix hound

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe User, type: :model do
-  describe 'document' do
-    context 'validations' do
+  describe "document" do
+    context "validations" do
       it { is_expected.to validate_presence_of(:email) }
     end
   end
 
   let(:user) { build(:user) }
 
-  it 'is databse authenticable' do
-    expect(user.valid_password?('password')).to be_truthy
+  it "is databse authenticable" do
+    expect(user.valid_password?("password")).to be_truthy
   end
 end
