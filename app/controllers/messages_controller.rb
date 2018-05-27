dfc000f901d3243a3721f06b64ec751d9571b43a
Robikos <r.a.kostrzewski@gmail.com>
2018-05-27 13:20:54 +0200

    Fix ActiveStorage settings
    HT-29

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
 class MessagesController < ApplicationController
-	def index
+  def index
     @message.images.attach(params[:images])
   end
 
   def create
-  	binding.pry
+    # binding.pry
     message = Message.create!(message_params)
-    #message.image.attach(params[:message][:image])
+    # message.image.attach(params[:message][:image])
     head :ok
+    # indentations lol
   end
 
   private
 
   def message_params
-  	params.require(:message).permit(:body, :image)
+    params.require(:message).permit(:body, :image)
   end
 end
