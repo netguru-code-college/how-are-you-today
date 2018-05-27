9dbd74d46bb3b8cf97ff5aaec322f96e19113ed8
Robikos <r.a.kostrzewski@gmail.com>
2018-05-27 15:23:09 +0200

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_27_081725) do
+ActiveRecord::Schema.define(version: 2018_05_27_104936) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2018_05_27_081725) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "memberships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "chat_room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_room_id"], name: "index_memberships_on_chat_room_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.bigint "user_id"
@@ -80,6 +89,8 @@ ActiveRecord::Schema.define(version: 2018_05_27_081725) do
   end
 
   add_foreign_key "chat_rooms", "users"
+  add_foreign_key "memberships", "chat_rooms"
+  add_foreign_key "memberships", "users"
   add_foreign_key "messages", "chat_rooms"
   add_foreign_key "messages", "users"
 end
