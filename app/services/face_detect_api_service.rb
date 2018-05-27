014a4f505dfc1da91e7a6e4f808cc9cffcbbc14c
Michał Łasecki <michal.lasecki@firma.interia.pl>
2018-05-27 13:09:11 +0200

    fix syntax

--- app/services/face_detect_api_service.rb
+++ app/services/face_detect_api_service.rb
@@ -7,21 +7,23 @@ class FaceDetectApiService
 
   def call
     uri = URI("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect")
-    uri.query = URI.encode_www_form({
-        'returnFaceId' => 'true',
-        'returnFaceLandmarks' => 'false',
-        'returnFaceAttributes' => 'emotion,age,gender',
-        'scores' => 'true'
-    })
+    uri.query = URI.encode_www_form(
+      {
+        "returnFaceId" => "true",
+        "returnFaceLandmarks" => "false",
+        "returnFaceAttributes" => "emotion,age,gender",
+        "scores" => "true"
+      }
+    )
 
     request = Net::HTTP::Post.new(uri.request_uri)
     request["Content-Type"] = "application/json"
     request["Ocp-Apim-Subscription-Key"] = Rails.application.secrets.emotion_api_key
 
     request.body = "{\"url\":\"#{@image_path}\"}"
-    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https") do |http|
       http.request(request)
     end
-    result = JSON.parse(response.body)
+    JSON.parse(response.body)
   end
 end
