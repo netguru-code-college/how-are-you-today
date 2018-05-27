3b21c52739b072da77c2d46cb62018a448857a28
Michał Łasecki <michal.lasecki@firma.interia.pl>
2018-05-27 16:00:50 +0200

    quick fix

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,11 +8,12 @@ class MessagesController < ApplicationController
   def create
     message = Message.create!(message_params)
     message.image.attach(params[:message][:image])
-    FaceDetectApiService.new(url_for(message.image)).call
+    if message.image.present?
+      FaceDetectApiService.new(url_for(message.image)).call
 
-    ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel",
+      ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel",
                                  message: "Detection done: Your link is: #{Rails.root.join("public/pdfs", "hello.pdf")}"
-
+    end
     head :ok
   end
 
